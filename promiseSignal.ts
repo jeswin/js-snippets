db5296e4f8f23cbee2f0f66f80acff20aa07a55e
Jeswin <jeswinpk@agilehead.com>
2020-12-08 08:15:28 +0530

    Update promiseSignal.ts

--- promiseSignal.ts
+++ promiseSignal.ts
@@ -1,11 +1,14 @@
-function promiseSignal<T>() {
+export default function promiseSignal<T>() {
   let resolve: (
     value?: T | PromiseLike<T> | undefined
   ) => void = undefined as any;
+
   let reject: (reason?: any) => void = undefined as any;
+
   const promise: Promise<T> = new Promise<T>((fnResolve, fnReject) => {
-    resolve = fnResolve;
+    resolve = fnResolve as (value?: T | PromiseLike<T> | undefined) => void;
     reject = fnReject;
   });
+
   return { promise, resolve, reject };
 }
