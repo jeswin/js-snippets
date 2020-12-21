0f865f11290062ef37636010daca4e0424361028
Jeswin <jeswinpk@agilehead.com>
2020-12-21 12:47:52 +0530

    Update debounce.ts

--- debounce.ts
+++ debounce.ts
@@ -1,8 +1,10 @@
-export function debounce<T extends Function>(cb: T, wait = 20) {
+/* eslint-disable */
+export function debounce<T extends Function>(cb: T, wait = 20) : any {
   let h: any = 0;
-  let callable = (...args: any) => {
+  const callable = (...args: any) => {
     clearTimeout(h);
     h = setTimeout(() => cb(...args), wait);
   };
   return (callable as any) as T;
 }
+
