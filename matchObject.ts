8f37ab340a34ea7df286be0a45125c17fc9b2cbd
Jeswin <jeswinpk@agilehead.com>
2021-05-01 14:41:31 +0530

    Create matchObject.ts

---
+++ matchObject.ts
@@ -0,0 +1,30 @@
+export default function matchObject(obj: any, source: any) {
+  if (typeof source !== typeof obj) {
+    return false;
+  }
+  if (Array.isArray(source)) {
+    if (!Array.isArray(obj)) {
+      return false;
+    } else {
+      for (const item of source) {
+        if (!obj.some((bItem) => matchObject(item, bItem))) {
+          return false;
+        }
+      }
+    }
+  } else if (Object(source) === source) {
+    if (Object(obj) !== obj) {
+      return false;
+    } else {
+      for (const valueKey of Object.keys(source)) {
+        if (!matchObject(source[valueKey], obj[valueKey])) {
+          return false;
+        }
+      }
+    }
+  } else {
+    if (source !== obj) {
+      return false;
+    }
+  }
+}
